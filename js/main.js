c2fd832985f93abc54ad234d5291e3395ecb30d8
Mohamed Ben Hamza <mohamedbenhamza90@gmail.com>
2021-11-07 17:55:30 +0100

    One page responsive

--- js/main.js
+++ js/main.js
@@ -15,3 +15,31 @@ typewriter
   .typeString('Wordpress developer')
   .pauseFor(1000)
   .start();
+
+
+
+var config= document.querySelector('.pics');
+var mixer= mixitup(config)
+var mixer = mixitup(config, {
+    selectors: {
+        target: '.blog-item'
+    },
+    animation: {
+        duration: 200
+    }
+});
+
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+var rootElement = document.documentElement;
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+  
+    scrollToTopBtn.style.display = "block";
+    
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
